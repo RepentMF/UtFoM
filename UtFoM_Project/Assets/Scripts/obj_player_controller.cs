c151ba9343a7dce248948c0a02b40358fbc344a8
RepentMF <repentmfps@gmail.com>
2017-09-22 22:26:53 -0400

    Started testing obj_player.
    Made spr_player, obj_player_controller and edited some settings.

---
+++ UtFoM_Project/Assets/Scripts/obj_player_controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obj_player_controller : MonoBehaviour
+{
+    public float moveSpeed;
+    public string hori = "Horizontal";
+    public string vert = "Vertical";
+
+    // Use this for initialization
+    void Start()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetAxisRaw(hori) > 0.5f || Input.GetAxisRaw(hori) < -0.5f)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw(hori) * moveSpeed * Time.deltaTime, 0f, 0f));
+        }
+
+        if (Input.GetAxisRaw(vert) > 0.5f || Input.GetAxisRaw(vert) < -0.5f)
+        {
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw(vert) * moveSpeed * Time.deltaTime, 0f));
+        }
+    }
+}
