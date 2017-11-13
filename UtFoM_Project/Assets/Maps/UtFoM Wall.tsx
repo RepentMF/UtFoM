5ffa5e17c700339d8f2e7fa6801a6ed9741bbfad
Corben14 <32212954+Corben14@users.noreply.github.com>
2017-11-12 20:13:34 -0500

    Finishing up level 1
    Catching up

---
+++ UtFoM_Project/Assets/Maps/UtFoM Wall.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="UtFoM Wall" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="../Documents/GitHub/UtFoM/UtFoM_Project/Assets/Art/Wall..png" width="36" height="36"/>
+</tileset>
