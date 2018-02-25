8fe2e10c270e10219ec7da61b79d10c66bab4e38
Corben14 <brandont25@gmail.com>
2018-02-25 11:43:42 -0500

    SimplerSkel
    A basic outline of the dungeon and town order and when the weapons would be acquired.

--- UtFoM_Project/Assets/Maps/UtFoM Floor.tsx
+++ UtFoM_Project/Assets/Maps/UtFoM Floor.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="UtFoM Floor" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../Documents/GitHub/UtFoM/UtFoM_Project/Assets/Art/Floor..png" width="36" height="36"/>
+ <image source="../Art/Floor..png" width="36" height="36"/>
 </tileset>
--- UtFoM_Project/Assets/Maps/UtFoM Player.tsx
+++ UtFoM_Project/Assets/Maps/UtFoM Player.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="UtFoM Player" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../Documents/GitHub/UtFoM/UtFoM_Project/Assets/Art/Player..png" width="36" height="36"/>
+ <image source="../Art/Player..png" width="36" height="36"/>
 </tileset>
