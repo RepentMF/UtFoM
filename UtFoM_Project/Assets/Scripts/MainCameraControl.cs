e6c3537e96f9dc736adcf08e2049f4d0e13b8748
mnelson16 <mnelson16@ggc.edu>
2018-04-21 20:08:34 -0400

    Made progress with player and camera prototypes.

---
+++ UtFoM_Project/Assets/Scripts/MainCameraControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraControl : MonoBehaviour 
+{
+    private static bool exists = false;
+    private Vector3 targetPosition;
+    public float moveSpeed;
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () 
+	{
+        DontDestroyOnLoad(transform.gameObject);
+
+        if (!exists)
+        {
+            exists = true;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        targetPosition = new Vector3(target.transform.position.x, target.transform.position.y,
+            transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+	}
+}
---
+++ UtFoM_Project/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour 
+{
+    private string hori = "Horizontal", vert = "Vertical", respawnName;
+    public int currentAtk, currentHealth, currentSpd, extraAtk, extraSpd, extraHealth,
+        maxHealth, weaponAtk;
+    public float deadzone, hSpeed, moveSpeed, runSpeed, stickAngle, stickDir, stickX, stickY, 
+        vSpeed, walkSpeed;
+    private ArrayList inv;
+    private Animator animator;
+    private Rigidbody2D body;
+
+    void movePlayer()
+    {
+        if ((Input.GetAxis("RT") > 0) || (Input.GetKey("r")))
+        {
+            moveSpeed = runSpeed;
+        }
+        else
+        {
+            moveSpeed = walkSpeed;
+        }
+    }
+
+    void determineAngle()
+    {
+        if (System.Math.Abs(Input.GetAxisRaw(hori)) >= deadzone || System.Math.Abs(Input.GetAxisRaw(vert)) >= deadzone)
+        {
+            if (System.Math.Sign(Input.GetAxisRaw(hori)) > 0 && System.Math.Sign(Input.GetAxisRaw(vert)) > 0)
+            {
+                stickAngle = stickDir * 180 / (float)System.Math.PI;
+            }
+            else if (System.Math.Sign(Input.GetAxisRaw(hori)) < 0 && System.Math.Sign(Input.GetAxisRaw(vert)) > 0)
+            {
+                stickAngle = (stickDir * 180 / (float)System.Math.PI) + 180;
+            }
+            else if (System.Math.Sign(Input.GetAxisRaw(hori)) < 0 && System.Math.Sign(Input.GetAxisRaw(vert)) < 0)
+            {
+                stickAngle = (stickDir * 180 / (float)System.Math.PI) + 180;
+            }
+            else if (System.Math.Sign(Input.GetAxisRaw(hori)) > 0 && System.Math.Sign(Input.GetAxisRaw(vert)) < 0)
+            {
+                stickAngle = (stickDir * 180 / (float)System.Math.PI) + 360;
+            }
+            else if (Input.GetAxisRaw(hori) == 0 && System.Math.Sign(Input.GetAxisRaw(vert)) == 1)
+            {
+                stickAngle = 90;
+            }
+            else if (System.Math.Sign(Input.GetAxisRaw(hori)) == -1 && Input.GetAxisRaw(vert) == 0)
+            {
+                stickAngle = 180;
+            }
+            else if (Input.GetAxisRaw(hori) == 0 && System.Math.Sign(Input.GetAxisRaw(vert)) == -1)
+            {
+                stickAngle = 270;
+            }
+            else
+            {
+                stickAngle = stickDir * 180 / (float) System.Math.PI;
+            }
+        }
+    }
+
+    void animatePlayer()
+    {
+        determineAngle();
+        animator.SetFloat("Stick's Angle", stickAngle);
+    }
+
+	// Use this for initialization
+	void Start() 
+	{
+        animator = GetComponent<Animator>();
+        body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+        movePlayer();
+        animatePlayer();
+	}
+}
