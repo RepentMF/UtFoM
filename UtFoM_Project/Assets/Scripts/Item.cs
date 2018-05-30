af909fa7a16887dbe8ffc6650f23af089f58e065
mnelson16 <mnelson16@ggc.edu>
2018-05-30 11:49:35 -0400

    Various changes and adds
    Added:
    -Stamina
    
    Changed:
    -how dodging and jumping work

--- UtFoM_Project/Assets/Scripts/Item.cs
+++ UtFoM_Project/Assets/Scripts/Item.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Item : MonoBehaviour 
 {
-
+    
 	// Use this for initialization
 	void Start () 
 	{
--- UtFoM_Project/Assets/Scripts/PlayerControl.cs
+++ UtFoM_Project/Assets/Scripts/PlayerControl.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public bool grounded = true, moving = false, dodging = false;
-    public int currentAtk, currentHealth, currentSpd, extraAtk, extraSpd, extraHealth,
-       maxHealth, weaponAtk;
-    public float deadzone, dodgeTime = 0.5f, dodgeTimer = 0.0f, hSpeed, moveSpeed, runSpeed,
-        stickAngle, lastStickAngle, stickDir, stickX, stickY, vSpeed, walkSpeed;
+    public bool dodging = false, grounded = true, moving = false, running = false;
+    public int currentAtk, currentHealth, currentSpd, maxStm = 9, weaponAtk;
+    public float cripple = -9.0f, crippleTimer, crippleSpd, currentStm, deadzone, dodgeTime = 0.5f, 
+        dodgeTimer = 0.0f, dodgeSpeed, dodgeSpeedH, dodgeSpeedV, hSpeed, moveSpeed, 
+        runSpeed, stickAngle, lastStickAngle, runTime = 1.0f, stickDir, stickX, stickY, vSpeed, walkSpeed;
     private string hori = "Horizontal", vert = "Vertical", respawnName;
     public Animator animator;
     public Rigidbody2D body;
     public SpriteRenderer sprite;
+    public Item quickItem;
     public List<Item> inv;
     public GameObject interactable;
 
@@ -21,9 +22,13 @@ public class PlayerControl : MonoBehaviour
     void MovePlayer()
     {
         // Checks to see if the player is running or not
-        if (/*(Input.GetAxis("RT") > 0) || */(Input.GetKey("r")))
+        if (running)
         {
             moveSpeed = runSpeed;
+        }
+        else if (currentStm == -1)
+        {
+            moveSpeed = crippleSpd;
         }
         else
         {
@@ -36,15 +41,15 @@ public class PlayerControl : MonoBehaviour
         if (stickX == 0 && stickY == 0)
         {
             stickDir = 0;
-            moving = false;
         }
         else
         {
             stickDir = (float)System.Math.Atan(Input.GetAxisRaw(vert) / Input.GetAxisRaw(hori));
-            moving = true;
         }
         hSpeed = (float)System.Math.Cos(stickDir) * moveSpeed * System.Math.Sign(Input.GetAxisRaw(hori));
         vSpeed = (float)System.Math.Sin(stickDir) * moveSpeed;
+
+        
         
         // Set Vertical Speed's numerical sign
         if (Input.GetAxisRaw(hori) < 0 && Input.GetAxisRaw(vert) < 0)
@@ -54,6 +59,16 @@ public class PlayerControl : MonoBehaviour
         else if (Input.GetAxisRaw(hori) < 0 && Input.GetAxisRaw(vert) > 0)
         {
             vSpeed *= -1;
+        }
+
+        // Make sure player is "moving"
+        if (Mathf.Abs(hSpeed) > 0 || Mathf.Abs(vSpeed) > 0)
+        {
+            moving = true;
+        }
+        else
+        {
+            moving = false;
         }
 
         // Applies movement changes
@@ -79,8 +94,17 @@ public class PlayerControl : MonoBehaviour
         if (dodgeTimer == 0.0f)
         {
             // Make the player dodge
-            hSpeed *= 2;
-            vSpeed *= 2;
+            dodgeSpeedH = (float)System.Math.Cos(stickDir) * dodgeSpeed * System.Math.Sign(Input.GetAxisRaw(hori));
+            dodgeSpeedV = (float)System.Math.Sin(stickDir) * dodgeSpeed;
+            if (Input.GetAxisRaw(hori) < 0 && Input.GetAxisRaw(vert) < 0)
+            {
+                dodgeSpeedV *= System.Math.Sign(Input.GetAxisRaw(vert));
+            }
+            else if (Input.GetAxisRaw(hori) < 0 && Input.GetAxisRaw(vert) > 0)
+            {
+                dodgeSpeedV *= -1;
+            }
+            body.velocity = new Vector2(dodgeSpeedH, dodgeSpeedV);
             sprite.color = new Color(0f, 1f, 0f);
         }
 
@@ -93,13 +117,13 @@ public class PlayerControl : MonoBehaviour
             // Make the player stop dodging and change the boolean for the animator
             dodgeTimer = 0.0f;
             dodging = false;
+            grounded = true;
             animator.SetBool("dodging", dodging);
             hSpeed = 0f;
             vSpeed = 0f;
+            body.velocity = new Vector2(hSpeed, vSpeed);
             sprite.color = new Color(1f, 1f, 1f);
         }
-        // Apply the dodge changes to the player
-        body.velocity = new Vector2(hSpeed, vSpeed);
     }
 
     // Determine the angle for the player's movement
@@ -126,7 +150,6 @@ public class PlayerControl : MonoBehaviour
             else if (System.Math.Sign(Input.GetAxisRaw(hori)) > 0 && System.Math.Sign(Input.GetAxisRaw(vert)) < 0)
             {
                 // Fourth Quadrant
-                // Running dodge works here
                 stickAngle = (stickDir * 180 / (float)System.Math.PI) + 360;
             }
             else if (Input.GetAxisRaw(hori) == 0 && System.Math.Sign(Input.GetAxisRaw(vert)) == 1)
@@ -142,13 +165,11 @@ public class PlayerControl : MonoBehaviour
             else if (Input.GetAxisRaw(hori) == 0 && System.Math.Sign(Input.GetAxisRaw(vert)) == -1)
             {
                 // 270 Degrees
-                // Running dodge works here
                 stickAngle = 270;
             }
             else
             {
-                // 0 or 360 Degrees                
-                // Running dodge works here
+                // 0 or 360 Degrees
                 stickAngle = stickDir * 180 / (float) System.Math.PI;
             }
             // Sets lastStickAngle to stickAngle, in case stickAngle changes but you need to use a previous 
@@ -214,13 +235,21 @@ public class PlayerControl : MonoBehaviour
         animator = GetComponent<Animator>();
         body = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
-
         if (SceneManager.GetActiveScene().name == "Main")
         {
-            // Sets up player's first direction and speeds
+            // Sets up player's first stats and direction
+            currentSpd = 10;
             walkSpeed = currentSpd;
+            crippleSpd = currentSpd * 0.6f;
             runSpeed = currentSpd * 1.5f;
+            dodgeSpeed = currentSpd * 1.75f;
             lastStickAngle = 270;
+
+            maxStm = 9;
+            currentStm = maxStm;
+            cripple = -9.0f;
+            dodgeTime = 0.5f;
+            runTime = 1.0f;
         }
 	}
 
@@ -229,11 +258,51 @@ public class PlayerControl : MonoBehaviour
     {
         if (SceneManager.GetActiveScene().name == "Main")
         {
+            if (/*(Input.GetAxis("RT") > 0) || */(Input.GetKeyDown("r")))
+            {
+                if (running)
+                {
+                    running = false;
+                }
+                else
+                {
+                    running = true;
+                }
+            }
+
             // Check if the player is not dodging
-            if (!dodging)
+            if (!dodging && grounded)
             {
                 MovePlayer();
                 AnimatePlayer();
+                
+                if (running && moving && currentStm >= 0)
+                {
+                    crippleTimer = cripple;
+
+                    currentStm -= Time.deltaTime;
+                }
+                else if (currentStm < 0)
+                {
+                    currentStm = -1;
+                    running = false;
+                    crippleTimer += Time.deltaTime;
+                    if (crippleTimer >= runTime)
+                    {
+                        currentStm = maxStm;
+                        crippleTimer = cripple;
+                    }
+                }
+                else if (currentStm < maxStm)
+                {
+                    crippleTimer = cripple;
+
+                    currentStm += Time.deltaTime;
+                    if (currentStm > maxStm)
+                    {
+                        currentStm = maxStm;
+                    }
+                }
 
                 // Check if the player tries to interact with something
                 if (Input.GetKeyDown("e") && interactable != null)
@@ -244,15 +313,39 @@ public class PlayerControl : MonoBehaviour
                         interactable.gameObject.SetActive(false);
                     }
                 }
-                // Then check if the player tries to dodge
-                else if (Input.GetKeyDown("f"))
+                // Then check if the player tries to dodge or jump
+                else if (Input.GetKeyDown("f") && currentStm > 0)
                 {
+                    // Dodge here
                     dodging = true;
+                    currentStm -= 3;
+                    if (currentStm < 0)
+                    {
+                        currentStm = -1;
+                    }
+                }
+                else if (Input.GetKeyDown("j") && currentStm > 0)
+                {
+                    // Jump here
+                    grounded = false;
+                    currentStm -= 3;
+                    if (currentStm < 0)
+                    {
+                        currentStm = -1;
+                    }
+                }
+                else if (Input.GetKeyDown("i"))
+                {
+                    // Use shortcut item here
+                    if (quickItem != null)
+                    {
+                        //quickItem.Use();
+                    }
                 }
             }
             else
             {
-                // Make player dodge
+                // Make player dodge or jump
                 DodgePlayer();
             }
         }
