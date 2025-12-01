f37202ef5853430d469c65cb9635b7b7a3743614
Matthew Nelson <repentmfps@gmail.com>
2025-12-01 10:31:31 -0500

    adding changes from G-Branch on InvController

--- Scripts/UniversalScripts/InventoryController.gd
+++ Scripts/UniversalScripts/InventoryController.gd
@@ -4,11 +4,13 @@ var inventory = []
 var currentWeapon
 
 func _ready():
-	inventory.push_back(new_weapon_add("fists", "put 'em up", "LightPunch", "HeavyPunch", "JugglePunch"))
-	inventory.push_back(new_weapon_add("knife", "it's a knife", "LightKnife", "", ""))
-	inventory.push_back(new_weapon_add("hammer", "hammer c:", "LightHammer", "HeavyHammer", "JuggleHammer"))
-	inventory.push_back(new_weapon_add("blade", "swish", "LightBlade", "", "JuggleBlade"))
-	#inventory.push_back(new_weapon_add("sword", "swoosh", "LightSword", "HeavySword", ""))
+	inventory.push_back(new_weapon_add("fists", "put 'em up", "LightPunch", "HeavyPunch", "JugglePunch")) # heavy, no looping
+	inventory.push_back(new_weapon_add("knife", "it's a knife", "LightKnife", "", "")) # light, no looping
+	inventory.push_back(new_weapon_add("hammer", "hammer c:", "LightHammer", "HeavyHammer", "JuggleHammer")) # juggle 1 > juggle 2 > juggle 3 > heavy; juggle 1 > juggle 2 > heavy; juggle 1 > heavy; heavy
+	inventory.push_back(new_weapon_add("sword", "swoosh", "LightSword", "HeavySword", "")) # light 1 > light 2 > heavy; light 1 > heavy; heavy
+	inventory.push_back(new_weapon_add("claw", "santa", "LightClaw", "LightClaw", "LightClaw")) # light, no looping
+	inventory.push_back(new_weapon_add("blade", "swish", "LightBlade", "", "JuggleBlade")) # light 1 > light 2 > juggle; light 1 > juggle; juggle
+	
 	currentWeapon = inventory[1]
 
 func new_weapon_add(iName, description, light, heavy, juggle):
