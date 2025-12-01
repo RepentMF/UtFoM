64f189b02ca0b758db55202b3bcadeb1b8c1a29c
Matthew Nelson <repentmfps@gmail.com>
2025-12-01 10:42:37 -0500

    almost done making everything as one

--- Scripts/HUDScripts/HUDController.gd
+++ Scripts/HUDScripts/HUDController.gd
@@ -2,9 +2,11 @@ extends Control
 
 #sets various sprites for hud
 var fist_texture = load("res://Sprites/HUD/Fist.png")
-var sword_texture = load("res://Sprites/HUD/Sword.png")
 var knife_texture = load("res://Sprites/HUD/Knife.png")
 var hammer_texture = load("res://Sprites/HUD/Hammer.png")
+var sword_texture = load("res://Sprites/HUD/Sword.png")
+var claw_texture = load("res://Sprites/HUD/Claw.png")
+var blade_texture = load("res://Sprites/HUD/Blade.png")
 
 
 func _physics_process(_delta: float) -> void:
@@ -13,9 +15,13 @@ func _physics_process(_delta: float) -> void:
 	match get_parent().get_parent().currentWeapon.name:
 		"fists":
 			%WeaponIcon.texture = fist_texture
-		"sword":
-			%WeaponIcon.texture = sword_texture
 		"knife":
 			%WeaponIcon.texture = knife_texture
 		"hammer":
 			%WeaponIcon.texture = hammer_texture
+		"sword":
+			%WeaponIcon.texture = sword_texture
+		"claw":
+			%WeaponIcon.texture = claw_texture
+		"blade":
+			%WeaponIcon.texture = blade_texture
--- Scripts/UniversalScripts/InventoryController.gd
+++ Scripts/UniversalScripts/InventoryController.gd
@@ -4,8 +4,8 @@ var inventory = []
 var currentWeapon
 
 func _ready():
-	inventory.push_back(new_weapon_add("fists", "put 'em up", "LightPunch", "HeavyPunch", "JugglePunch")) # heavy, no looping
-	inventory.push_back(new_weapon_add("knife", "it's a knife", "LightKnife", "", "")) # light, no looping
+	inventory.push_back(new_weapon_add("fists", "put 'em up", "HeavyPunch", "HeavyPunch", "HeavyPunch")) # heavy, no looping
+	inventory.push_back(new_weapon_add("knife", "it's a knife", "LightKnife", "LightKnife", "LightKnife")) # light, no looping
 	inventory.push_back(new_weapon_add("hammer", "hammer c:", "LightHammer", "HeavyHammer", "JuggleHammer")) # juggle 1 > juggle 2 > juggle 3 > heavy; juggle 1 > juggle 2 > heavy; juggle 1 > heavy; heavy
 	inventory.push_back(new_weapon_add("sword", "swoosh", "LightSword", "HeavySword", "")) # light 1 > light 2 > heavy; light 1 > heavy; heavy
 	inventory.push_back(new_weapon_add("claw", "santa", "LightClaw", "LightClaw", "LightClaw")) # light, no looping
