cfe602cb5a224da3f71287d05403ff00494ba149
RepentMF <repentmfps@gmail.com>
2026-04-29 19:53:23 -0400

    fixed killroom puzzle

--- Scripts/PuzzleScripts/PuzzleKillroom.gd
+++ Scripts/PuzzleScripts/PuzzleKillroom.gd
@@ -3,40 +3,32 @@ extends Node2D
 var solved = false
 var killroomList
 var killroomCount
-var childrenNames = []
+var enemyNames = []
 var missingCount = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	killroomList = get_meta("KillroomList")
 	for child in get_parent().get_children():
-		childrenNames.push_front(child.name)
-		print(child.name)
-	for enemy in killroomList:
-		if enemy.name.contains("")
+		for enemy in killroomList:
+			if child.name.contains(enemy):
+				enemyNames.push_front(enemy)
+				print(enemy)
+	print()
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	#if !solved:
-	#	attempt_puzzle_solve()
-	pass
-
 func attempt_puzzle_solve():
-	for enemy in killroomList:
-		if childrenNames.has(enemy.name):
-			missingCount += 1
-	if missingCount == killroomCount:
+	if enemyNames.is_empty():
 		solved = true
 		print("door open!")
 	else:
-		print(missingCount, ", ", killroomCount)
+		print(enemyNames.size())
 
 func _on_child_exiting_tree(node):
-	var removed = false
-	for child in childrenNames:
-		if child.name == node.name:
-			childrenNames.remove(child)
+	for enemy in enemyNames:
+		if node.name.contains(enemy):
+			enemyNames.erase(enemy)
+			print(enemy)
 			break
 	attempt_puzzle_solve()
 	pass # Replace with function body.
